6225ad7a6682ff985d394c25db772f288809d1ae
elbeeraa <ecarcelzam1@educacion.navarra.es>
2025-10-23 13:43:29 +0200

    corregido

--- UT4Ejer12TablaMultiplicar/src/main/java/org/example/Main.java
+++ UT4Ejer12TablaMultiplicar/src/main/java/org/example/Main.java
@@ -5,7 +5,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(i + " " + i*2+ " " + i*3+ " " + i*4+ " " + i*5+ " " + i*6+ " " + i*7+ " " + i*8+ " " + i*9+ " " + i*10);
+            for (int k = 1; k <= 10; k++) {
+                int r = i * k;
+                System.out.printf("%4d",r);
+            }
+            System.out.println();
         }
     }
 }
