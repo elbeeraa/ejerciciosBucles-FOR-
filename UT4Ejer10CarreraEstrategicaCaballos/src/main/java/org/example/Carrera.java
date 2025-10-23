49e9d082484f83f8938286ffce6902b9c470891c
elbeeraa <ecarcelzam1@educacion.navarra.es>
2025-10-23 09:51:13 +0200

    hechos

--- UT4Ejer10CarreraEstrategicaCaballos/src/main/java/org/example/Carrera.java
+++ UT4Ejer10CarreraEstrategicaCaballos/src/main/java/org/example/Carrera.java
@@ -25,22 +25,42 @@ public class Carrera {
             System.out.print("RESPUESTA:");
             int respuesta1 = Integer.parseInt(teclado.nextLine());
             caballoJugador1.correr(respuesta1);
-            System.out.println("Tu caballo tiene: " + caballoJugador1.getPotencia() + " de potencia.");
+            System.out.println("Tu caballo tiene \uD83D\uDC0E : " + caballoJugador1.getPotencia() + " de potencia.");
 
             System.out.println("-".repeat(40));
 
             Scanner teclado2 = new Scanner(System.in);
             System.out.println("Jugador 2 elige una opción: ");
-            System.out.println("- Velocidad máxima [1]");
-            System.out.println("- Velocidad normal [2]");
-            System.out.println("- Velocidad mínima [3]");
-            System.out.println("- Descansar [4]");
+            System.out.println("- Velocidad máxima (- 15 potencia) [1]");
+            System.out.println("- Velocidad normal (- 10 potencia) [2]");
+            System.out.println("- Velocidad mínima (- 5 potencia) [3]");
+            System.out.println("- Descansar (+ 10 potencia) [4]");
             System.out.print("RESPUESTA:");
             int respuesta2 = Integer.parseInt(teclado2.nextLine());
             caballoJugador2.correr(respuesta2);
-            System.out.println("Tu caballo tiene: " + caballoJugador2.getPotencia() + " de potencia.");
+            System.out.println("Tu caballo tiene \uD83D\uDC0E : " + caballoJugador2.getPotencia() + " de potencia.");
 
-            System.out.println("-".repeat(40));
+
+            if(respuesta1 != 4) {
+                System.out.println("S " + "🏁" + "-".repeat(100));
+                System.out.println("T " + "🏁" + " ".repeat(caballoJugador1.getDistanciaRecorrida()) + "\uD83C\uDFC7");
+                System.out.println("A " + "🏁" + "-".repeat(100));
+            } else {
+                System.out.println("S " + "🏁" + "-".repeat(100));
+                System.out.println("T " + "🏁" + " ".repeat(caballoJugador1.getDistanciaRecorrida()) + "\uD83C\uDFC7" + "Zzz");
+                System.out.println("A " + "🏁" + "-".repeat(100));
+            }
+
+            if(respuesta2 != 4) {
+                System.out.println("R " + "🏁" + "-".repeat(100));
+                System.out.println("T " + "🏁" + " ".repeat(caballoJugador2.getDistanciaRecorrida()) + "\uD83C\uDFC7\uD83C\uDFFE");
+                System.out.println("  " + "🏁" + "-".repeat(100));
+            }else{
+                System.out.println("R " + "🏁" + "-".repeat(100));
+                System.out.println("T " + "🏁" + " ".repeat(caballoJugador2.getDistanciaRecorrida()) + "\uD83C\uDFC7\uD83C\uDFFE"+"Zzz");
+                System.out.println("  " + "🏁" + "-".repeat(100));
+            }
+
         }
         if(caballoJugador1.getDistanciaRecorrida() > caballoJugador2.getDistanciaRecorrida()) {
             System.out.println("El jugador 1 ha ganado!");
